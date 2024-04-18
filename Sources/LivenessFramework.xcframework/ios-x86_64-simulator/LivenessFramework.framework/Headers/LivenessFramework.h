3aff15816b137ae9bc3ee115c564b7a1f4d846d5
Talib <talib.shabbir@jamipartners.com>
2024-04-18 18:20:31 +0500

    Initial Commit

---
+++ Sources/LivenessFramework.xcframework/ios-x86_64-simulator/LivenessFramework.framework/Headers/LivenessFramework.h
@@ -0,0 +1,18 @@
+//
+//  LivenessFramework.h
+//  LivenessFramework
+//
+//  Created by iOS Dev JP on 4/16/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LivenessFramework.
+FOUNDATION_EXPORT double LivenessFrameworkVersionNumber;
+
+//! Project version string for LivenessFramework.
+FOUNDATION_EXPORT const unsigned char LivenessFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LivenessFramework/PublicHeader.h>
+
+
